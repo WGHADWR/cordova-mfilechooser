8a0f34437910aaa07c73743ad68da6592a6d6245
unknown <wghadwr@gmail.com>
2018-06-25 11:11:09 +0800

    ''

--- www/mfilechooser.js
+++ www/mfilechooser.js
@@ -1,10 +1,6 @@
 var exec = require('cordova/exec');
 
-var mfilechooser = {};
-
-mfilechooser.open = function (arg0, success, error) {
+exports.open = function (arg0, success, error) {
     console.log('Cordova file chooser ...');
     exec(success, error, 'mfilechooser', 'open', [arg0]);
 };
-
-module.exports = mfilechooser;
