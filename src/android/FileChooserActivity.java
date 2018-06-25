77c43a167f73350e9451a917ee48cdc6916cf750
unknown <wghadwr@gmail.com>
2018-06-25 16:59:20 +0800

    ''

--- src/android/FileChooserActivity.java
+++ src/android/FileChooserActivity.java
@@ -1,14 +1,159 @@
 package com.gx.filechooser;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+
+import com.gx.filechooser.common.FileUtils;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class FileChooserActivity extends Activity {
+
+    private TextView title;
+    private String path = Environment.getExternalStorageDirectory().getAbsolutePath();
+
+    private List<Map<String, Object>> listItems;
+    private SimpleAdapter adapter;
+
+    private List<String> selectedFiles = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(this.getApplication().getResources().getIdentifier(
                 "activity_filechooser", "layout", getApplication().getPackageName()));
+
+        this.title = findViewById(R.id.title);
+        this.title.setText(this.path);
+
+        final ListView listView = this.findViewById(R.id.list_view);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Map<String, Object> selectedItem = listItems.get(i);
+                String fileName = selectedItem.get("title").toString();
+                String selectFile;
+                if (path.equals("/")) {
+                    selectFile = path + fileName;
+                } else {
+                    selectFile = path + File.separator + fileName;
+                }
+                if ((new File(selectFile)).isDirectory()) {
+                    path = selectFile;
+                    clearSelectedFiles();
+                    loadItems(selectFile);
+                    return;
+                }
+                addOrRemoveFile(view, selectFile);
+            }
+        });
+
+        String[] from = { "image", "title" };
+        int[] to = { R.id.imageView, R.id.textView2 };
+
+        this.listItems = this.getListItems(this.path);
+        this.adapter = new SimpleAdapter(
+                this, this.listItems, R.layout.listview_item, from, to);
+        listView.setAdapter(this.adapter);
+    }
+
+    private void clearSelectedFiles() {
+        this.selectedFiles.clear();
+        Button okButton = findViewById(R.id.btnOK);
+        okButton.setText("确定");
+        okButton.setEnabled(false);
+        okButton.setTextColor(Color.parseColor("#C3C3C3"));
+    }
+
+    private void addOrRemoveFile(View view, String selectFile) {
+        if (selectedFiles.contains(selectFile)) {
+            selectedFiles.remove(selectFile);
+            view.setBackgroundColor(Color.parseColor("#FFFFFF"));
+        } else {
+            selectedFiles.add(selectFile);
+            view.setBackgroundColor(Color.parseColor("#ECEEF0"));
+        }
+
+        if (this.selectedFiles.isEmpty()) {
+            this.clearSelectedFiles();
+        } else {
+            Button okButton = findViewById(R.id.btnOK);
+            okButton.setText("确定(" + this.selectedFiles.size() + ")");
+            okButton.setEnabled(true);
+            okButton.setTextColor(Color.WHITE);
+        }
+    }
+
+    private void loadItems(String path) {
+        this.title.setText(path);
+        if (!path.equals("/")) {
+            path += "/";
+        }
+        List<Map<String, Object>> data = getListItems(path);
+        listItems.clear();
+        if (data != null && data.size() > 0) {
+            listItems.addAll(data);
+        }
+        adapter.notifyDataSetChanged();
+    }
+
+    public void onBack(View view) {
+        if (this.path.equals("/")) {
+            this.finish();
+            return;
+        }
+        this.clearSelectedFiles();
+
+        File file = new File(this.path);
+        this.path = file.getParent();
+        this.loadItems(file.getParent());
+    }
+
+    public void onOK(View source) {
+        Intent intent = new Intent();
+        intent.putExtra("selectedFiles", this.selectedFiles.toArray());
+        this.setResult(RESULT_OK, intent);
+        this.finish();
+    }
+
+    private List<Map<String, Object>> getListItems(String parent) {
+        List<Map<String, Object>> items = new ArrayList<Map<String, Object>>();
+        File[] subFiles = getSubFiles(parent);
+        if (subFiles == null || subFiles.length == 0) {
+            return items;
+        }
+        List<File> files = FileUtils.sort(Arrays.asList(subFiles));
+        files = FileUtils.sort(files);
+
+        for (File file : files) {
+            Map<String, Object> item = new HashMap<String, Object>();
+            if (file.isDirectory()) {
+                item.put("image", R.drawable.folder_default);
+            } else {
+                item.put("image", R.drawable.file_x72);
+            }
+            item.put("title", file.getName());
+
+            items.add(item);
+        }
+        return items;
+    }
+
+    private File[] getSubFiles(String parent) {
+        return (new File(parent)).listFiles();
     }
 }
